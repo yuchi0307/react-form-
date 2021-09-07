ecb1c9db3c41331d1e93c63955a409e8e7d14136
yuchi0307 <gelee0307@gmail.com>
2021-09-07 22:16:01 +0800

    Managing a List Of Users via State

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import AddUsers from './components/Users/AddUsers'
+import React, { useState } from "react";
+import AddUsers from "./components/Users/AddUsers";
+import UserList from "./components/Users/UserList";
 
 
-function App() {
 
+function App(props) {
+  const [usersInfo, setUsersInfo] = useState([]);
+  
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersInfo((prevUsers) => {
+     return [...prevUsers, { id:Math.random().toString(), name:uName, age:uAge} ];
+    });
+
+  };
+  
 
   return (
     <div>
-      <section>
-        <AddUsers />
-      </section>
-      <section>
-        {/* {userInfo} */}
+      <section >
+        <AddUsers onAddUser={addUserHandler} />
+        <UserList users={usersInfo} />        
       </section>
     </div>
   );
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -7,7 +7,6 @@ import Button from "../UI/Button";
 const AddUsers = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-  const [enteredValue, setEnterdValue] = useState({ enteredName, enteredAge });
 
   const onNameChangeHandler = (e) => {
     if (e.target.value.trim().length > 0) {
@@ -16,30 +15,42 @@ const AddUsers = (props) => {
   };
 
   const onAgeChangeHandler = (e) => {
-    if (e.target.value > 0) {
+     if (e.target.value > 0) {
       setEnteredAge(e.target.value.toString());
     }
   };
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    if (enteredName !== "" && enteredAge > 0) {
-      setEnterdValue(enteredName, enteredAge);
+
+    const addUserHandler = (event) =>{
+      event.preventDefault();
+      if(enteredName.trim().length === 0 || enteredAge.trim().length === 0)
+      {
+        return;
+      }
+
+      if(+enteredAge < 0)
+      {
+        return
+      }
+      props.onAddUser(enteredName,enteredAge)
+      setEnteredName('');
+      setEnteredAge('');
+
     }
-    props.onAddUser(enteredValue);
-  };
+ 
+  
 
   return (
     <Card className={styles.input}>
         {/* 這邊的className是由Card組件prop而來的 
         因為Card 是我們客製化的組件，不如普通html會有className等屬性在react中讀得懂css應用，
         客製化組件完全不知道className什麼碗糕。*/}
-      <form onSubmit={submitHandler}>
+      <form onSubmit={addUserHandler}>
         <label>User Name</label>
-        <input id="username" type="text" onChange={onNameChangeHandler}></input>
+        <input id="username" type="text" value={enteredName} onChange={onNameChangeHandler}></input>
         <br></br>
         <label>Age</label>
-        <input id="age" type="number" onChange={onAgeChangeHandler}></input>
+        <input id="age" type="number" value={enteredAge} onChange={onAgeChangeHandler}></input>
         <br></br>
         <Button type="submit">Add User</Button>
       </form>
